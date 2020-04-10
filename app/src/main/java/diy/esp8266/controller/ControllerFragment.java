99f236ed48f35a92a9bfecab43763ade9b974387
stefan <stefanrandelzhofer@gmail.com>
2020-04-11 00:36:45 +0200

    Have fun with the new icon

--- app/src/main/java/diy/esp8266/controller/ControllerFragment.java
+++ app/src/main/java/diy/esp8266/controller/ControllerFragment.java
@@ -15,7 +15,7 @@ public class ControllerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        return inflater.inflate(R.layout.fragment_controller, container, false);
     }
 
     public void onViewCreated(@NonNull final View view, Bundle savedInstanceState) {
