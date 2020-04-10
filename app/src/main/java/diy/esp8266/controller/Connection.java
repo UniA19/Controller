d7359cc23ab19c7e02d7c276ead92133a3416eb7
SBester001 <SBester001@t-online.de>
2020-04-11 00:55:53 +0200

    fixed communication

--- app/src/main/java/diy/esp8266/controller/Connection.java
+++ app/src/main/java/diy/esp8266/controller/Connection.java
@@ -1,101 +1,96 @@
 package diy.esp8266.controller;
 
 import android.content.Context;
-import android.widget.Toast;
 
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-class Connection extends Thread {
+class Connection extends Thread
+{
+    private static final String HOSTIP = "192.168.4.1";
+    private static final int PORT = 100;
 
     int leftX = 0, leftY = 0, rightX = 0, rightY = 0;
-    private int leftFront, leftBack, rightFront, rightBack;
 
     private static final double minPercent = 0.5;
     private static final int coolDown = 150;
 
-    private Socket socket;
     private Context context;
 
+    private Socket socket;
     private OutputStream out;
     private PrintWriter output;
 
-    private boolean sendData = false;
+    private boolean connencted = false;
+    private boolean updated = false;
 
     Connection(Context context)
     {
         this.context = context;
-        //start();
+        connect();
+        start();
     }
 
     @Override
     public void run() {
         super.run();
-        try {
-            socket = new Socket("192.168.4.1", 100);
-            out = socket.getOutputStream();
-            output = new PrintWriter(out);
-        } catch (Exception e) {
-            Toast.makeText(context, "Could not connect to ESP8266! Please restart the App", Toast.LENGTH_LONG).show();
-            e.printStackTrace();
-        }
-
         while (true) {
-            if (sendData) {
-                output.write("<" + leftFront + "|" + rightFront + "|" + leftBack + "|" + rightBack + ">");
-                output.flush();
-                System.out.println("Sent to Server: <" + leftFront + "|" + rightFront + "|" + leftBack + "|" + rightBack + ">");
-                sendData = false;
-                try { Thread.sleep(coolDown); } catch (InterruptedException ignored) {}
+            if (updated) {
+                send("<" + leftX + "|" + leftY + "|" + rightX + "|" + rightY + ">");
+                updated = false;
             }
+            try {sleep(coolDown);} catch (InterruptedException ignored) {}
         }
-    }
-
-    private void update()
-    {
-        //todo delete
-        System.out.println("Update to: leftX: " + leftX + " leftY: " + leftY + " rightX: " + rightX + " rightY: " + rightY);
-        int base = Math.abs((leftY - JoystickFAB.RADIUS) * 100 / (2 * JoystickFAB.RADIUS)); //leftY
-        System.out.println("Base: " + base);
-        leftFront = leftBack = rightFront = rightBack = base;
-        //leftX not jet used
-
-        //use right
-        if (rightX < 0) {
-            leftFront -= base * Math.abs(((double)rightX / JoystickFAB.RADIUS) * (minPercent / 2));
-            leftBack -= base * Math.abs(((double)rightX / JoystickFAB.RADIUS) * (minPercent / 2));
-        }
-
-        if (rightX > 0) {
-            rightFront -= base * Math.abs(((double)rightX / JoystickFAB.RADIUS) * (minPercent / 2));
-            rightBack -= base * Math.abs(((double)rightX / JoystickFAB.RADIUS) * (minPercent / 2));
-        }
-
-        if (rightY < 0) {
-            leftFront -= base * Math.abs(((double)rightY / JoystickFAB.RADIUS) * (minPercent / 2));
-            rightFront -= base * Math.abs(((double)rightY / JoystickFAB.RADIUS) * (minPercent / 2));
-        }
-
-        if (rightY > 0) {
-            leftBack-= base * Math.abs(((double)rightY / JoystickFAB.RADIUS) * (minPercent / 2));
-            rightBack -= base * Math.abs(((double)rightY / JoystickFAB.RADIUS) * (minPercent / 2));
-        }
-
-        sendData = true;
     }
 
     void setLeft(int leftX, int leftY)
     {
         this.leftX = leftX;
         this.leftY = leftY;
-        update();
+        updated = true;
     }
 
     void setRight(int rightX, int rightY)
     {
         this.rightX = rightX;
         this.rightY = rightY;
-        update();
+        updated = true;
+    }
+
+    public void connect()
+    {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    if (output != null) output.close();
+                    if (out != null) out.close();
+                    if (socket != null) socket.close();
+                    socket = new Socket(HOSTIP, PORT);
+                    out = socket.getOutputStream();
+                    output = new PrintWriter(out);
+                    connencted = true;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    connencted = false;
+                }
+            }
+        });
+        thread.start();
+    }
+
+    void send(final String data) {
+        if (connencted) {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    output.write(data);
+                    output.flush();
+                    System.out.println(data);
+                }
+            });
+            thread.start();
+        }
     }
 }
